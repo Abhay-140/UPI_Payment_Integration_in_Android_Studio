96e369182398b6054622cb4114a678d80d1ba6ae
Abhay Pendariya <147203178+Abhay-140@users.noreply.github.com>
2025-04-25 18:02:25 +0530

    Update Data.kt

--- app/src/main/java/com/upipaymentintegration/Data.kt
+++ app/src/main/java/com/upipaymentintegration/Data.kt
@@ -1,8 +1,6 @@
 package com.upipaymentintegration
 
-//val UPI_ID = "6263476472@ptaxis"
-val UPI_ID = "ojharahul7283@okhdfcbank"
-val Name = "ojharahul7283@okhdfcbank"
-//val Name = "Abhay Pendariya"
-val Merchant_Code = "1234"
+val UPI_ID = "Enter your Merchant UPI Id"
+val Name = "Add your Payee name"
+val Merchant_Code = "Enter your Merchant code"
 
